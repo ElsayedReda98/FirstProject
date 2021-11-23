63a1d6028c6e04dadc614f872697c0f73126160e
ElsayedReda98 <94932279+ElsayedReda98@users.noreply.github.com>
2021-11-23 22:30:59 +0200

    Add files via upload

---
+++ Stocks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Stocks
+    {
+        public int StoreId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+
+
+    }
+}
